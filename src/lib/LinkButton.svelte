9c459cffe3e5e839ec6c98ec814ed8c08542c6ee
Martin Dennhardt <martin@dennhardt.de>
2023-03-04 16:16:48 +0100

    fix: :lipstick: add missing focus outline to button

--- src/lib/LinkButton.svelte
+++ src/lib/LinkButton.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <a
-  class="inline-block text-light-text bg-light-element dark:text-dark-text dark:bg-dark-element p-[0.25em_1.5em] rounded-[0.25em] shadow-md"
+  class="inline-block text-light-text bg-light-element dark:text-dark-text dark:bg-dark-element p-[0.25em_1.5em] rounded-[0.25em] shadow-md focus:ring-1 focus:ring-blue"
   {href}
 >
   <slot />
